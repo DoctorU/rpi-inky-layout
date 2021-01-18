653a8f0cd4e9dcb12ea0a0e44b1c4cdcb5bdd6a6
Stephen Hobdell <hobdell.stephen@gmail.com>
2021-01-18 23:48:25 +0000

    Layout is stable for some test scenarios. Still debugging 3 horizontal sub-layouts

--- Layout.py
+++ Layout.py
@@ -1,77 +1,157 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageChops
 from math import floor
+from random import randint
+import numpy
 
 class Layout:
 
     DEFAULT_PALETTE = (255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252
-    def __init__(self, dimensions=(250,122), packingMode='h', border=0, rotationMode=0):
-        self.dimensions = dimensions 
-        self._packingMode = packingMode
-        self.border = border
-        self.rotation = 90*rotationMode
+
+    def __init__(self, size=(250,122), packingMode='h', border=(0,0), depth=0):
+        self.size = size
+        self._dimensions = tuple(s*1.0 for s in size)
+        self._middle = tuple(dim/2 for dim in size)
+        self.topleft = (0,0)
+        self.packingMode = packingMode
+        print("type of border: ", type(border))
+        if isinstance(border, int):
+            self.borderWidth = border
+            self.borderColour = 2
+        elif isinstance(border, tuple):
+            self.borderWidth = border[0]
+            self.borderColour = border[1]
+        elif not isinstance(border, None):
+            raises ("border is not an allowed type:{t}".format(t=type(border)))
         self._children = []
-        self._reset()
+        self._image = None
+        self._depth=depth
+        self._id = randint(1024,8192)
 
-    def _reset(self):
-        self._image = Image.new("P", self.dimensions, 1)
-        self._image.putpalette(self.DEFAULT_PALETTE)
-        self._drawable = True;
+    def setImage(self, image):
+        self._image = image.crop((0,0)+self.size)
 
+    def getDimensions(self):
+        return self.size;
+
+    def addLayer(self, border=1):
+        #add a child Layer to the child list, then resize them all
+        childLayer = Layout(depth=self._depth + 1, border=border) # make it at least the same size
+        self._children.append(childLayer)
+        self._updateChildren()
+        return childLayer
+
+    def resize(self, size):
+        print(self, "resizing to ", size)
+        self.size = size
+        if self._image:
+            self._image = self._image.crop((0,0) + size)
+        self._updateChildren()
+
+    def reposition(self, size, topleft=(0,0)):
+        self.topleft = topleft
+        self.resize(size)
+
+    def __str__(self):
+        return "{id}/{depth}:{tl}:{dim}:{mid}:{children}".format(
+            id = self._id,
+            depth = self._depth,
+            tl = self.topleft,
+            mid = self._middle,
+            dim = self.size,
+            children = len(self._children)
+        )
+
+    """
+        _updateChildren
+
+        Iterates over all children of this Layout, and resizes them, and sets their positions
+    """
+    def _updateChildren(self):
+        childCount = len(self._children)
+        if childCount > 0: # only update when there's children present
+            size = self.size
+            borderWidth = self.borderWidth
+            if self.packingMode == 'v':
+                size = tuple(reversed(size))
+#
+            childMid = (size[0]/childCount, size[1])
+            childMid = tuple(dim/2 for dim in childMid)
+            size = (((childMid[0]*2)-(borderWidth*(childCount+1))), ((childMid[1]-borderWidth)*2))
+            size = tuple(round(dim) for dim in size)
+
+            if self.packingMode == 'v':
+                size = tuple(reversed(size))
+
+            print (" self - size:", self.size,
+                " border:",borderWidth,
+                " packing:", self.packingMode,
+                "child - count:", childCount,
+                " size:", size)
+
+            print("gonna resize", childCount, self.packingMode,
+            "\n border:", borderWidth,
+            " child new dimension: ", size)
+            [child.resize(size) for child in self._children]
+            [child._setMiddle(childMid) for child in self._children]
+
+    def _setMiddle(self, middle):
+        self._middle = middle
+
+    def draw(self):
+        if not self._image:
+            self._image = Image.new("P", self.size, self._depth)
+        self._drawChildren()
+        # draw the border
+        draw = ImageDraw.Draw(self._image)
+        if self.borderWidth:
+            self._drawBorder(draw)
+
+    def _drawBorder(self, draw):
+        w = self.borderWidth
+        c = self.borderColour
+        draw.rectangle([(0,0),self.size], outline=c, width=w)
+        count = len(self._children)
+        for i,child in enumerate(self._children):
+            if(self.packingMode == 'h'):
+                xy = [(self.size[0]/count*(i+1), 0), (self.size[0]/count*(i+1),self.size[1]) ]
+                draw.line(xy, c, w)
+            if(self.packingMode == 'v'):
+                xy = [ (0, self.size[1]/count*(i+1)), (self.size[0],self.size[1]/count*(i+1)) ]
+                draw.line(xy, c, w)
+
+    def _drawChildren(self):
+        [child._drawChildren() for child in self._children]
+        [self._drawChildOnParent(child,index) for index,child in enumerate(self._children)]
+
+    def _drawChildOnParent(self, child, index):
+        print("_drawChildOnParent", self, child, index)
+        # child contains the image to paste; index helps calculate the offset
+        size = child.size
+        tl = child.topleft
+        print("child:{c}".format(c=child))
+        if self.packingMode == 'h':
+            size = tuple(reversed(size))
+        # SOMETHING IS WRONG HERE!
+        childPos = ( self.borderWidth, self.borderWidth * (index+1) + (size[1] * index))
+        if self.packingMode == 'h':
+            childPos = tuple(reversed(childPos))
+        print ("index:{i}, size:{s}, pos:{pos}".format(i=index, s=size, pos=childPos))
+
+        if not child._image:
+            print ("WARNING: NO IMAGE SET ON THIS CHILD", child);
+        else:
+            print("Child position: ", childPos, child.size, child._image.size,
+                "\non parent: ", self.size,
+                "\ndiff:image.size self-child:", numpy.subtract(self._image.size, child._image.size),
+                "diff:size self-child:", numpy.subtract(self.size, child.size))
+            self._image.paste(child._image, childPos)
+            #self._image.paste(child._image, (200,0))
 
     def write(self, fp):
         self.draw()
         self._image.save(fp)
-
-    """ 
-        get_draw - returns an ImageDraw object for drawing on, only if this has no children.
-        Otherwise, throws an exception stating that it can't be drawn on.
-    """
-    def get_ImageDraw(self):
         if self._children:
-            raise Exception("You cannot draw on layers that have children")
-        return ImageDraw.Draw(self._image)
-
-    def draw(self):
-        self.report("drawing {dim}".format(dim=self.dimensions))
-        self._updateChildren()
-        return self._image
+            [ child.write(str(index) +"-"+ fp) for index, child in enumerate(self._children)]
 
     def report(self, message):
         print(message)
-
-    def resize(self, dimensions):
-        self.dimensions = dimensions
-        self._updateChildren()
-
-    def addLayer(self, border=1):
-        #add a child Layer to the child list, then resize them all
-        childLayer = Layout()
-        self._reset()
-        self._drawable = False;
-        self._children.append(childLayer)
-        self._updateChildren()
-        self.draw()
-        return childLayer
-
-    def _updateChildren(self):
-        childCount = len(self._children)
-        if childCount > 0:
-            if self._packingMode == 'h':
-                bordersW = (childCount-1+2) * self.border
-                bordersH =  2 * self.border
-                newChildW = floor(self.dimensions[0]/childCount) - bordersW
-                newChildH = self.dimensions[1] - bordersH
-
-            if self._packingMode == 'v':
-                bordersW =  2 * self.border
-                bordersH = (childCount-1+2) * self.border
-                newChildW = self.dimensions[0] - bordersW
-                newChildH = floor(self.dimensions[1]/childCount) - bordersH
-                newdim = (newChildW, newChildH)
-                
-            print(childCount, self._packingMode, bordersW, bordersH, newChildW, newChildH)
-            newChildDims = [(child.resize((newChildW, newChildH)) ) for child in self._children]
-
-            
-
-
--- layout_empty_test.py
+++ layout_empty_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from math import floor
 from Layout import Layout
+from random import randint
 import unittest
+from PIL import Image, ImageDraw
 
 class TestStringMethods(unittest.TestCase):
 
@@ -10,83 +12,163 @@ class TestStringMethods(unittest.TestCase):
 
     def testCreateLayout(self):
         l = Layout( (200,100) )
-        self.assertEqual( (200,100), l.dimensions )
-        filename = "test1.png"
-        l.get_ImageDraw().text( (0,50), filename)
-        self.writeToFile(l, filename)
-        l.get_ImageDraw()
+        self.assertEqual( (200,100), l.size )
+
+        filename = 'testCreateLayout1.png'
+        # Create the img
+        img = self.NewImage(l)
+        # draw on it
+        draw = ImageDraw.Draw(img)
+        draw.text( (2,50), filename)
+        #set it on the layer
+        l.setImage(img)
+        # render it
+        l.draw()
+        #Write it
+        l.write(filename)
 
     def testAdd2ChildrenHorizontalPacking(self):
-        l = Layout( (200,100), 'h')
-        l.get_ImageDraw().text( (0,50), "main layout 1")
+        l = Layout( (200,100), 'h', border=2)
+        img = self.NewImage(l)
+        draw = self.NewDraw(img)
+        draw.text( (2,50), "main layout 1", 2)
+        l.setImage(img)
         layer1 = l.addLayer( )
-        layer1.get_ImageDraw().text( (0,50), "layer 1-1")
-        
-        self.assertEqual( (200,100), layer1.dimensions )
-        self.writeToFile(l, 'test2.png')
-        with self.assertRaises(Exception):
-            l.get_ImageDraw()
+
+        img = self.NewImage(layer1)
+        draw = self.NewDraw(img)
+        draw.text( (2,50), "layer 1-1", 2)
+        layer1.setImage(img)
+
+        self.assertEqual( (196, 96), layer1.size )
+        layer1.write( 'test2.png')
 
         layer2 = l.addLayer()
-        layer2.get_ImageDraw().text( (0,50), "layer 1-2")
-        self.assertEqual( (100,100), layer1.dimensions )
-        self.assertEqual( (100,100), layer2.dimensions )
-        self.writeToFile(l, 'test3.png')
-        with self.assertRaises(Exception):
-            l.get_ImageDraw()
+        img = self.NewImage(layer2)
+        draw = self.NewDraw(img)
+        draw.text( (2,50), "layer 1-2", 2)
+        layer2.setImage(img)
+
+        self.assertEqual( (94, 96), layer1.size )
+        self.assertEqual( (94, 96), layer2.size )
+        layer2.write( 'test3.png')
 
     def testAdd2ChildrenVerticalPacking(self):
         l = Layout( (200,100), 'v')
-        l.get_ImageDraw().text( (0,50), "main layout 2")
+
         layer1 = l.addLayer( )
-        layer1.get_ImageDraw().text( (0,50), "layer 2-1")
-        self.assertEqual( (200,100), layer1.dimensions )
-        with self.assertRaises(Exception):
-            l.get_ImageDraw()
+        self.assertEqual( (200,100), layer1.size )
 
         layer2 = l.addLayer()
-        layer2.get_ImageDraw().text( (0,50), "layer 1-2")
-        self.assertEqual( (200,50), layer1.dimensions )
-        self.assertEqual( (200,50), layer2.dimensions )
-        with self.assertRaises(Exception):
-            l.get_ImageDraw()
-            
+        self.assertEqual( (200,50), layer1.size )
+        self.assertEqual( (200,50), layer2.size )
+
     def testAddChildrenPackedVWithBorderEnabled(self):
         l = Layout( (200,300), 'v', 1 )
         layer1 = l.addLayer( )
-        self.assertEqual( (198,298), layer1.dimensions )
+        self.assertEqual( (198,298), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (198,147 ), layer1.dimensions )
+        self.assertEqual( (198,147 ), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (198,96 ), layer1.dimensions )
+        self.assertEqual( (198,96 ), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (198,70), layer1.dimensions )
+        self.assertEqual( (198,70), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (198,54), layer1.dimensions )
+        self.assertEqual( (198,54), layer1.size )
 
 
 
     def testAddChildrenPackedHWithBorderEnabled(self):
         l = Layout( (200,300), 'h', 1 )
         layer1 = l.addLayer( )
-        self.assertEqual( (198,298), layer1.dimensions )
+        self.assertEqual( (198,298), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (97, 298), layer1.dimensions )
+        self.assertEqual( (97, 298), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (62, 298), layer1.dimensions )
+        self.assertEqual( (63, 298), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (45, 298), layer1.dimensions )
+        self.assertEqual( (45, 298), layer1.size )
 
         layer1 = l.addLayer( )
-        self.assertEqual( (34, 298), layer1.dimensions )
+        self.assertEqual( (34, 298), layer1.size )
 
+        layer1 = l.addLayer()
+        self.assertEqual( (26, 298), layer1.size )
+
+        layer1 = l.addLayer()
+        self.assertEqual( (21, 298), layer1.size )
+
+        layer1 = l.addLayer()
+        self.assertEqual( (16, 298), layer1.size )
+
+
+    def testTestGenerating2Layers(self):
+        l = Layout( (400,100), 'h', border=3)
+        #self.addStuffToImage(l)
+        self.addStuffToImage(l, 0)
+
+        layer1 = l.addLayer()
+        self.addStuffToImage(layer1, 0)
+        layer2 = l.addLayer()
+        layer3 = l.addLayer()
+
+        self.addStuffToImage(layer2, 1)
+        self.addStuffToImage(layer3, 2)
+
+        print (l, layer1, layer2, layer3)
+
+        l.draw()
+        l.write("testgen-l.png")
+        layer1.write("testgen-l1.png")
+        layer2.write("testgen-l2.png")
+        layer3.write("testgen-l3.png")
+        print("/testTestGenerating2Layers")
+
+    def testTreeOfLayers(self):
+        l1 = Layout ( (400,200), 'h', border=5)
+        self.addStuffToImage(l1)
+
+        l2 = l1.addLayer(3)
+        self.addStuffToImage(l2)
+
+        l3 = l2.addLayer()
+        self.addStuffToImage(l3)
+
+        l1.draw()
+        l1.write("tree-l1.png")
+        l2.write("tree-l2.png")
+        l3.write("tree-l3.png")
+        self.assertEqual(l1.size, (400,200))
+        self.assertEqual(l2.size, (390,190))
+        self.assertEqual(l3.size, (384,184))
+
+    def NewImage(self, layout):
+        img = Image.new("P", layout.size, randint(0,2))
+        img.putpalette(Layout.DEFAULT_PALETTE)
+        return img
+
+    def NewDraw(self, img):
+        return ImageDraw.Draw(img)
+
+    def addStuffToImage(self, layout, childId=0):
+        img = self.NewImage(layout)
+        draw = self.NewDraw(img)
+
+        diff = -5
+        draw.rectangle((5,5) + tuple((dim - 5) for dim in layout.size) , 1)
+        text = "{id}\nchildId:{cid}\ndepth:{d}\ns:{size}".format(
+            id=layout._id,
+            cid=childId, d=layout._depth,size=layout.size)
+        draw.text((10,25), text)
+        layout.setImage(img)
+        layout.draw()
 
 if __name__ == '__main__':
     unittest.main()
