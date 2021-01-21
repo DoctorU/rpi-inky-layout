6bdb764e8b9a7813836b00a1097c06b21ba7acc7
Stephen Hobdell <hobdell.stephen@gmail.com>
2021-01-21 23:38:57 +0000

    #5: implemented rotation.

--- Layout.py
+++ Layout.py
@@ -1,39 +1,76 @@
+from enum import Enum, unique
 from PIL import Image, ImageDraw
 from math import floor
 from random import randint
 import numpy
 
+@unique
+class Rotation(Enum):
+    UP=0 # Normal
+    RIGHT=1 # rotate left 90°
+    DOWN=2 # rotate 180°
+    LEFT=3 # rotate right 90°
+
 class Layout:
+    """
+        Layout - a simple image layout manager for RPi Inky HATs.
+    """
 
     DEFAULT_PALETTE = (255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252
 
-    def __init__(self, size=(250,122), packingMode='h', border=(0,0), depth=0):
-        self.size = size
-        self._dimensions = tuple(s*1.0 for s in size)
+    def __init__(self, size=(250,122), packingMode='h', border=(0,0), depth=0, rotation=Rotation.UP):
+        """
+            Construct a new Layer.
+            :param:size: the size of the layer. default:(250,122) (the size of an Inky-pHAT).
+            :param:packingMode: the packing mode, one of 'h' or 'v'.
+            :param:border: a border definition.
+                If an int, then the width, and the colour is assumed to be '2'.
+                If a 2-tuple, then the first part is the width, and the second is the colour.
+            :param:depth: for tracking the tree hierarchy - for internal use only. Default:0.
+            :param:rotation: the type of rotation. This modifies the way that images are drawn, so that
+                UP becomes the default top, DOWN would render upside down, and left and right similarly modify
+                the rendering.
+
+        """
+        self.rotation = rotation
+        print (self.rotation, self.rotation.value, self.rotation.value %2)
+        if self.rotation.value % 2:
+            self.size = tuple(reversed(size)) # 1,3
+        else:
+            self.size = size
+        self.rotation_degrees = self.rotation.value * 90
+        #self._dimensions = tuple(s*1.0 for s in size)
         self._middle = tuple(dim/2 for dim in size)
         self.topleft = (0,0)
         self.packingMode = packingMode
-        print("type of border: ", type(border))
+
         if isinstance(border, int):
             self.borderWidth = border
             self.borderColour = 2
         elif isinstance(border, tuple):
-            self.borderWidth = border[0]
-            self.borderColour = border[1]
+            self.borderWidth, self.borderColour = border
         elif not isinstance(border, None):
             raises ("border is not an allowed type:{t}".format(t=type(border)))
+
         self._children = []
         self._image = None
-        self._depth=depth
+        self._depth = depth
         self._id = randint(1024,8192)
 
     def setImage(self, image):
+        """
+            setImage
+            Set the image on this layer after you've drawn it.
+            The image you set will be cropped to the Layer's size before being set on it.
+        """
         self._image = image.crop((0,0)+self.size)
 
-    def getDimensions(self):
-        return self.size;
-
     def addLayer(self, border=1):
+        """
+            addLayer(border=1)
+            Add a new layer to this Layer.
+            :param:border: the same as the constructor's border property.
+        """
         #add a child Layer to the child list, then resize them all
         childLayer = Layout(depth=self._depth + 1, border=border) # make it at least the same size
         self._children.append(childLayer)
@@ -41,7 +78,6 @@ class Layout:
         return childLayer
 
     def resize(self, size):
-        print(self, "resizing to ", size)
         self.size = size
         if self._image:
             self._image = self._image.crop((0,0) + size)
@@ -75,30 +111,19 @@ class Layout:
                 size = tuple(reversed(size))
 #
             childMid = (size[0]/childCount, size[1])
-            print(childMid)
             childMid = tuple(dim/2 for dim in childMid)
-            print(childMid)
+
             border0 = borderWidth*(childCount + 1)/childCount
             border1 = borderWidth*2
             size0 = childMid[0]*2
             size1 = childMid[1]*2
-            print (border0, border1, size0, size1)
+
             size = (size0 - border0, size1 - border1)
-            #size = (((childMid[0]*2)-(borderWidth*(childCount+1))), ((childMid[1]-borderWidth)*2))
             size = tuple(round(dim) for dim in size)
 
             if self.packingMode == 'v':
                 size = tuple(reversed(size))
 
-            print (" self - size:", self.size,
-                " border:",borderWidth,
-                " packing:", self.packingMode,
-                "child - count:", childCount,
-                " size:", size)
-
-            print("gonna resize", childCount, self.packingMode,
-            " border:", borderWidth,
-            " child new size: ", size)
             [child.resize(size) for child in self._children]
             [child._setMiddle(childMid) for child in self._children]
 
@@ -113,6 +138,7 @@ class Layout:
         draw = ImageDraw.Draw(self._image)
         if self.borderWidth:
             self._drawBorder(draw)
+        return self._image.rotate(-self.rotation_degrees, expand=1)
 
     def _drawBorder(self, draw):
         w = self.borderWidth
@@ -132,36 +158,26 @@ class Layout:
         [self._drawChildOnParent(child,index) for index,child in enumerate(self._children)]
 
     def _drawChildOnParent(self, child, index):
-        print("_drawChildOnParent", self, child, index)
+
         # child contains the image to paste; index helps calculate the offset
         size = child.size
         tl = child.topleft
-        print("child:{c}".format(c=child))
+
         if self.packingMode == 'v':
             size = tuple(reversed(size))
-        # SOMETHING IS WRONG HERE!
+
         borderOffset = self.borderWidth*(index+1)
         tlOffset0 = size[0]*index
         childPos = (borderOffset + tlOffset0,  self.borderWidth)
         if self.packingMode == 'v':
             childPos = tuple(reversed(childPos))
-        print ("index:{i}, size:{s}, pos:{pos}".format(i=index, s=size, pos=childPos))
 
         if not child._image:
             print ("WARNING: NO IMAGE SET ON THIS CHILD", child);
         else:
-            print("Child position: ", childPos, child.size, child._image.size,
-                "\non parent: ", self.size,
-                "\ndiff:image.size self-child:", numpy.subtract(self._image.size, child._image.size),
-                "diff:size self-child:", numpy.subtract(self.size, child.size))
             self._image.paste(child._image, childPos)
-            #self._image.paste(child._image, (200,0))
 
     def write(self, fp):
-        self.draw()
-        self._image.save(fp)
+        self.draw().save(fp)
         if self._children:
-            [ child.write(str(index) +"-"+ fp) for index, child in enumerate(self._children)]
-
-    def report(self, message):
-        print(message)
+            [ child.write(fp.replace(".", "-{i}.".format(i=index))) for index, child in enumerate(self._children)]
---
+++ layout_rotation_test.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+from Layout import Rotation
+from math import floor
+from Layout import Layout
+from random import randint
+import unittest
+from PIL import Image, ImageDraw
+
+import numpy
+
+class TestLayout(unittest.TestCase):
+
+    def writeToFile(self, layout, filename):
+        layout.write( filename )
+
+    def testRotation(self):
+        self.assertEqual(0, Rotation.UP.value)
+        self.assertEqual(1, Rotation.RIGHT.value)
+        self.assertEqual(3, Rotation.LEFT.value)
+        self.assertEqual(2, Rotation.DOWN.value)
+
+    def testCreateLayoutRotatedUp(self):
+        print("testing", Rotation.UP)
+        l = Layout( (200,100), rotation=Rotation.UP)
+        self.assertEqual((200,100), l.size)
+        self.assertEqual(0, l.rotation_degrees)
+
+    def testCreateLayoutRotatedLeft(self):
+        print ("testing", Rotation.LEFT)
+        l = Layout( (200,100), rotation=Rotation.LEFT)
+        self.assertEqual((100,200), l.size)
+        self.assertEqual(270, l.rotation_degrees)
+
+    def testCreateLayoutRotatedDown(self):
+        print ("testing", Rotation.DOWN)
+        l = Layout( (200,100), rotation=Rotation.DOWN)
+        self.assertEqual((200,100), l.size)
+        self.assertEqual(180, l.rotation_degrees)
+
+    def testCreateLayoutRotatedRight(self):
+        print ("testing", Rotation.RIGHT)
+        l = Layout( (200,100), rotation=Rotation.RIGHT)
+        self.assertEqual((100,200), l.size)
+        self.assertEqual(90, l.rotation_degrees)
+
+    def testLayoutRotatedRightAdd3Layers(self):
+        self.add3LayersAndRotate((300,100), Rotation.RIGHT)
+
+    def testLayoutRotatedLeftAdd3Layers(self):
+        self.add3LayersAndRotate((300,100), Rotation.LEFT)
+
+    def testLayoutRotatedDownAdd3Layers(self):
+        self.add3LayersAndRotate((100,300), Rotation.DOWN)
+
+    def testLayoutRotatedUpAdd3Layers(self):
+        self.add3LayersAndRotate((100,300), Rotation.UP)
+
+    def add3LayersAndRotate(self, size,  rotation):
+        l = Layout (size, packingMode='v', border=0, rotation=rotation)
+        l1 = l.addLayer()
+        l2 = l.addLayer()
+        l3 = l.addLayer()
+        img = Image.new("RGB", l.size, 0x000)
+        l.setImage(img)
+        self.assertEqual((100,100), l1.size)
+        self.assertEqual((100,100), l2.size)
+        self.assertEqual((100,100), l3.size)
+        img1 = Image.new("RGB", l1.size, 0xff0000) #red
+        self.drawTextOnImage("l1", img1)
+        l1.setImage(img1)
+        img2 = Image.new("RGB", l2.size, 0x000ff00) # green
+        self.drawTextOnImage("l2", img2)
+        l2.setImage(img2)
+        img3 = Image.new("RGB", l3.size, 0x0000ff) # blue
+        self.drawTextOnImage("l3", img3)
+        l3.setImage(img3)
+        l.write("test-rotated-{rot}-add-3-layers.png".format(rot=rotation.name))
+
+    def drawTextOnImage(self, text, img):
+        draw = ImageDraw.Draw(img)
+        tsize = draw.textsize(text)
+        tdiff = numpy.subtract(img.size, tsize)
+        thalf = numpy.divide(tdiff, 2)
+        tpos = tuple(thalf)
+
+        print("text pos", tpos, text)
+        draw.text(tpos, text, 0x000000)
+
+if __name__ == '__main__':
+    unittest.main()
