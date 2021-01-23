6ac5b0cfe1583bad72455dde1e8f9519d48c83af
Stephen Hobdell <hobdell.stephen@gmail.com>
2021-01-23 23:29:52 +0000

    #8: chore - linting.

--- layout_basics_test.py
+++ layout_basics_test.py
@@ -1,139 +1,128 @@
-#!/usr/bin/env python3
-from math import floor
 from Layout import Layout
 from random import randint
 import unittest
 from PIL import Image, ImageDraw
 
+
 class TestLayoutBasics(unittest.TestCase):
 
     @staticmethod
     def writeToFile(layout, filename):
-        layout.write( 'test/expected-images/' + filename )
+        layout.write('test/expected-images/' + filename)
 
     def testCreateLayout(self):
-        l = Layout( (200,100) )
-        self.assertEqual( (200,100), l.size )
+        layout = Layout((200, 100))
+        self.assertEqual((200, 100), layout.size)
 
         filename = 'testCreateLayout1.png'
-        # Create the img
-        img = self.NewImage(l)
-        # draw on it
+        img = self.NewImage(layout)
         draw = ImageDraw.Draw(img)
-        draw.text( (2,50), filename)
-        #set it on the layer
-        l.setImage(img)
-        # render it
-        l.draw()
-        #Write it
-        l.write(filename)
+        draw.text((2, 50), filename)
+        layout.setImage(img)
+        self.writeToFile(layout, filename)
 
     def testAdd2ChildrenHorizontalPacking(self):
-        l = Layout( (200,100), 'h', border=2)
-        img = self.NewImage(l)
+        layout = Layout((200, 100), 'h', border=2)
+        img = self.NewImage(layout)
         draw = self.NewDraw(img)
-        draw.text( (2,50), "main layout 1", 2)
-        l.setImage(img)
-        layer1 = l.addLayer( )
+        draw.text((2, 50), "main layout 1", 2)
+        layout.setImage(img)
+        layer1 = layout.addLayer()
 
         img = self.NewImage(layer1)
         draw = self.NewDraw(img)
-        draw.text( (2,50), "layer 1-1", 2)
+        draw.text((2, 50), "layer 1-1", 2)
         layer1.setImage(img)
 
-        self.assertEqual( (196, 96), layer1.size )
-        self.writeToFile(layer1,'test2.png')
+        self.assertEqual((196, 96), layer1.size)
+        self.writeToFile(layer1, 'test2.png')
 
-        layer2 = l.addLayer()
+        layer2 = layout.addLayer()
         img = self.NewImage(layer2)
         draw = self.NewDraw(img)
-        draw.text( (2,50), "layer 1-2", 2)
+        draw.text((2, 50), "layer 1-2", 2)
         layer2.setImage(img)
 
-        self.assertEqual( (97, 96), layer1.size )
-        self.assertEqual( (97, 96), layer2.size )
+        self.assertEqual((97, 96), layer1.size)
+        self.assertEqual((97, 96), layer2.size)
         self.writeToFile(layer2, 'test3.png')
 
     def testAdd2ChildrenVerticalPacking(self):
-        l = Layout( (200,100), 'v')
+        layout = Layout((200, 100), 'v')
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (200,100), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((200, 100), layer1.size)
 
-        layer2 = l.addLayer()
-        self.assertEqual( (200,50), layer1.size )
-        self.assertEqual( (200,50), layer2.size )
+        layer2 = layout.addLayer()
+        self.assertEqual((200, 50), layer1.size)
+        self.assertEqual((200, 50), layer2.size)
 
     def testAddChildrenPackedVWithBorderEnabled(self):
-        l = Layout( (200,300), 'v', 1 )
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,298), layer1.size )
+        layout = Layout((200, 300), 'v', 1)
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 298), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,148 ), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 148), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,99 ), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 99), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,74), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 74), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,59), layer1.size )
-
-
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 59), layer1.size)
 
     def testAddChildrenPackedHWithBorderEnabled(self):
-        l = Layout( (200,300), 'h', 1 )
-        layer1 = l.addLayer( )
-        self.assertEqual( (198,298), layer1.size )
+        layout = Layout((200, 300), 'h', 1)
+        layer1 = layout.addLayer()
+        self.assertEqual((198, 298), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (98, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((98, 298), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (65, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((65, 298), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (49, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((49, 298), layer1.size)
 
-        layer1 = l.addLayer( )
-        self.assertEqual( (39, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((39, 298), layer1.size)
 
-        layer1 = l.addLayer()
-        self.assertEqual( (32, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((32, 298), layer1.size)
 
-        layer1 = l.addLayer()
-        self.assertEqual( (27, 298), layer1.size )
+        layer1 = layout.addLayer()
+        self.assertEqual((27, 298), layer1.size)
 
-        layer1 = l.addLayer()
-        self.assertEqual( (24, 298), layer1.size )
-
+        layer1 = layout.addLayer()
+        self.assertEqual((24, 298), layer1.size)
 
     def testTestGenerating2Layers(self):
-        l = Layout( (400,100), 'h', border=3)
-        #self.addStuffToImage(l)
-        self.addStuffToImage(l, 0)
+        layout = Layout((400, 100), 'h', border=3)
+        self.addStuffToImage(layout, 0)
 
-        layer1 = l.addLayer()
+        layer1 = layout.addLayer()
         self.addStuffToImage(layer1, 0)
-        layer2 = l.addLayer()
-        layer3 = l.addLayer()
+        layer2 = layout.addLayer()
+        layer3 = layout.addLayer()
 
         self.addStuffToImage(layer2, 1)
         self.addStuffToImage(layer3, 2)
 
-        l.draw()
-        self.writeToFile(l, "testgen-l.png")
+        layout.draw()
+        self.writeToFile(layout, "testgen-l.png")
         self.writeToFile(layer1, "testgen-l1.png")
         self.writeToFile(layer2, "testgen-l2.png")
         self.writeToFile(layer3, "testgen-l3.png")
-        self.assertEqual( (129,94), layer1.size)
-        self.assertEqual( (129,94), layer2.size)
-        self.assertEqual( (129,94), layer3.size)
+        self.assertEqual((129, 94), layer1.size)
+        self.assertEqual((129, 94), layer2.size)
+        self.assertEqual((129, 94), layer3.size)
 
     def testTreeOfLayers(self):
-        l1 = Layout ( (400,200), 'h', border=5)
+        l1 = Layout((400, 200), 'h', border=5)
         self.addStuffToImage(l1)
 
         l2 = l1.addLayer(3)
@@ -146,12 +135,12 @@ class TestLayoutBasics(unittest.TestCase):
         self.writeToFile(l1, "tree-l1.png")
         self.writeToFile(l2, "tree-l2.png")
         self.writeToFile(l3, "tree-l3.png")
-        self.assertEqual(l1.size, (400,200))
-        self.assertEqual(l2.size, (390,190))
-        self.assertEqual(l3.size, (384,184))
+        self.assertEqual(l1.size, (400, 200))
+        self.assertEqual(l2.size, (390, 190))
+        self.assertEqual(l3.size, (384, 184))
 
     def NewImage(self, layout):
-        img = Image.new("P", layout.size, randint(0,2))
+        img = Image.new("P", layout.size, randint(0, 2))
         img.putpalette(Layout.DEFAULT_PALETTE)
         return img
 
@@ -162,14 +151,14 @@ class TestLayoutBasics(unittest.TestCase):
         img = self.NewImage(layout)
         draw = self.NewDraw(img)
 
-        diff = -5
-        draw.rectangle((5,5) + tuple((dim - 5) for dim in layout.size) , 1)
+        draw.rectangle((5, 5) + tuple((dim - 5) for dim in layout.size), 1)
         text = "{id}\nchildId:{cid}\ndepth:{d}\ns:{size}".format(
             id=layout._id,
-            cid=childId, d=layout._depth,size=layout.size)
-        draw.text((10,25), text)
+            cid=childId, d=layout._depth, size=layout.size)
+        draw.text((10, 25), text)
         layout.setImage(img)
         layout.draw()
+
 
 if __name__ == '__main__':
     unittest.main()
--- layout_packingBias_test.py
+++ layout_packingBias_test.py
@@ -11,7 +11,7 @@ class TestLayoutPackingBias(unittest.TestCase):
         layout.write('test/expected-images/' + filename)
 
     def test_getChildPackingBiasTotalAndBiasStart(self):
-        layout = Layout((100,100))
+        layout = Layout((100, 100))
         self.assertEqual(0, layout._getChildrenPackingBiasTotal())
 
         layout.addLayer(packingBias=3)
@@ -24,7 +24,7 @@ class TestLayoutPackingBias(unittest.TestCase):
         self.assertEqual(3, layout._getChildPackingBiasStart(1))
 
     def testLayoutWidthHorizontal(self):
-        layout = Layout((200, 10), packingMode='h', border=(0,0))
+        layout = Layout((200, 10), packingMode='h', border=(0, 0))
         layout1 = layout.addLayer(packingBias=3)
         self.assertEqual(200, layout1.size[0])
 
@@ -32,8 +32,8 @@ class TestLayoutPackingBias(unittest.TestCase):
 
         self.assertEqual(150, layout1.size[0])
         self.assertEqual(50, layout2.size[0])
-        self.assertEqual((0,0), layout1.topleft)
-        self.assertEqual((150,0), layout2.topleft)
+        self.assertEqual((0, 0), layout1.topleft)
+        self.assertEqual((150, 0), layout2.topleft)
         self.writeImage(layout, "testLayoutWidthHorizontal.png")
 
     def testLayoutWidthVertical(self):
@@ -46,25 +46,26 @@ class TestLayoutPackingBias(unittest.TestCase):
 
         self.assertEqual(150, layout1.size[1])
         self.assertEqual(50, layout2.size[1])
-        self.assertEqual((0,0), layout1.topleft)
-        self.assertEqual((0,150), layout2.topleft)
+        self.assertEqual((0, 0), layout1.topleft)
+        self.assertEqual((0, 150), layout2.topleft)
         self.writeImage(layout, "testLayoutWidthVertical.png")
 
     def testWrite(self):
-        layout = Layout((200,200))
+        layout = Layout((200, 200))
         layout1 = layout.addLayer(packingBias=1)
         layout2 = layout.addLayer(packingBias=3)
 
         def setImage(layout, colour, text):
             img = Image.new("RGB", layout.size, colour)
             draw = ImageDraw.Draw(img)
-            draw.text((0,0), text)
+            draw.text((0, 0), text)
             layout.setImage(img)
 
         setImage(layout1, 0xff0000, "layout1")
         setImage(layout2, 0x0088ff, "layout2")
         setImage(layout, 0x000000, "layout")
         self.writeImage(layout, "test-packingBias.png")
+
 
 if __name__ == '__main__':
     unittest.main()
