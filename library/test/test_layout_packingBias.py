31446eda3c5398e9ce935e6d4ef279df2745e8c9
Stephen Hobdell <hobdell.stephen@gmail.com>
2021-02-21 17:16:05 +0000

    fix: issue #35: additional test.

--- library/test/test_layout_packingBias.py
+++ library/test/test_layout_packingBias.py
@@ -48,13 +48,13 @@ class TestLayoutPackingBias(unittest.TestCase):
         print(layout1)
         print(layout2)
 
-        self.assertEqual((150, 10), layout2.size)
         self.assertEqual((50, 10), layout1.size)
+        self.assertEqual((150, 10), layout2.size)
         self.assertEqual((0, 0), layout1.topLeft)
         self.assertEqual((50, 0), layout2.topLeft)
         self.writeImage(layout, "testPackingBiasHorizontal1_3.png")
 
-    def testPackingBiasVertical(self):
+    def testPackingBiasVertical3_1(self):
 
         layout = Layout((10, 200), packingMode='v', border=(0, 0))
         layout1 = layout.addLayer(packingBias=3)
@@ -66,7 +66,21 @@ class TestLayoutPackingBias(unittest.TestCase):
         self.assertEqual((10, 50), layout2.size)
         self.assertEqual((0, 0), layout1.topLeft)
         self.assertEqual((0, 150), layout2.topLeft)
-        self.writeImage(layout, "testPackingBiasVertical.png")
+        self.writeImage(layout, "testPackingBiasVertical3_1.png")
+
+    def testPackingBiasVertical1_3(self):
+
+        layout = Layout((10, 200), packingMode='v', border=(0, 0))
+        layout1 = layout.addLayer(packingBias=1)
+        self.assertEqual((10, 200), layout1.size)
+
+        layout2 = layout.addLayer(packingBias=3)
+
+        self.assertEqual((10, 50), layout1.size)
+        self.assertEqual((10, 150), layout2.size)
+        self.assertEqual((0, 0), layout1.topLeft)
+        self.assertEqual((0, 50), layout2.topLeft)
+        self.writeImage(layout, "testPackingBiasVertical1_3.png")
 
     def testWrite(self):
         layout = Layout((200, 200))
